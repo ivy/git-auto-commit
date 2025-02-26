86f7dfac5ba7bd693c3904e31045b0e4e84d3730
Ivy Evans <ivy@ivyevans.net>
2025-02-26 18:46:07 +0000

    Add DefaultBranch and Log functions to git package
    - Implement DefaultBranch function to fetch the default branch name.
    - Add Log function to retrieve git log with patch details for changes
      from the default branch to HEAD.

--- util/git/git.go
+++ util/git/git.go
@@ -1,6 +1,11 @@
 package git
 
-import "github.com/ivy/git-auto-commit/util/exec"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/ivy/git-auto-commit/util/exec"
+)
 
 // Status returns the output of `git status` command. It returns the status as a
 // string and an error if the command fails.
@@ -19,6 +24,33 @@ func Diff(cached bool) (string, error) {
 		args = append(args, "--cached")
 	}
 	cmd := exec.Command("git", args...)
+	out, err := cmd.Output()
+	return string(out), err
+}
+
+// DefaultBranch returns the name of the default branch. It returns the branch
+// name as a string and an error if the command fails.
+func DefaultBranch() (string, error) {
+	cmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
+	out, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(string(out)), nil
+}
+
+// Log returns the output of `git log --patch` command. It returns the log as a
+// string and an error if the command fails.
+func Log() (string, error) {
+	defaultBranch, err := DefaultBranch()
+	if err != nil {
+		return "", err
+	}
+
+	cmd := exec.Command(
+		"git", "log", "--patch",
+		fmt.Sprintf("%s/%s...%s", "origin", defaultBranch, "HEAD"),
+	)
 	out, err := cmd.Output()
 	return string(out), err
 }
